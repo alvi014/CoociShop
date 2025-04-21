eff329fe390b2231272b423b656cb5cd653b48b8
alvi014 <avictorza06@gmail.com>
2025-04-20 19:02:22 -0600

    Permisos de CORS a netlify

--- backend/server.js
+++ backend/server.js
@@ -53,7 +53,8 @@ app.use(cors({
     const allowedOrigins = [
       "http://localhost:5500",
       "http://127.0.0.1:5500",
-      "https://coocishop.onrender.com"
+      "https://coocishop.onrender.com",
+      "https://coocishop.netlify.app"
     ];
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
